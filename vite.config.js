992e19f48c7ef5ad74aa57c0328f6c26f47fb1f6
Mohammed Hanees <hanees@Mohammeds-MacBook-Air.local>
2025-08-05 23:43:32 +0530

    changes by mac 5-8-25

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-  import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
-  import tailwindcss from '@tailwindcss/vite'
+  // import { defineConfig } from 'vite'
+  // import react from '@vitejs/plugin-react'
+  // import tailwindcss from '@tailwindcss/vite'
 
 
-  // https://vite.dev/config/
-  export default defineConfig({
-    plugins: [tailwindcss(),],
-  })
+  // // https://vite.dev/config/
+  // export default defineConfig({
+  //   plugins: [tailwindcss(),],
+  // })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './'  // ← Add this line
+})
